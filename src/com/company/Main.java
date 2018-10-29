b05e04bf879ba9f126d57331990c6d03ebf30b67
ugryn513 <ugryn513@ukr.net>
2018-10-29 18:18:29 +0200

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,10 +5,24 @@ import java.lang.Math;
 public class Main {
 
     public static void main(String[] args) {
-        int A, B, C;
+        int A = 0;
+        int B = 1;
+
         for (int i = 3; i < 10; i++)
         {
-            A = A + Math.pow(i, 2);
+            A = A + (int) Math.pow(i, 2);
+
         }
+
+        System.out.println("A = " + A);
+
+        for (int i = 2; i < 9; i++)
+        {
+            B = B * i;
+        }
+
+        System.out.println("B = " + B);
+        System.out.println("C = " + (A + B));
     }
 }
+
