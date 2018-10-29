d1413cf9b1628f7a84e456aa1c1801be994641a2
ugryn513 <ugryn513@ukr.net>
2018-10-29 09:18:45 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.lang.Math;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int A, B, C;
+        for (int i = 3; i < 10; i++)
+        {
+            A = A + Math.pow(i, 2);
+        }
+    }
+}
